0c64d5a9742bc9a25f8f919a113c7e7c549f01be
Shakil Ahmed <shakilbuet63@gmail.com>
2021-07-27 11:14:32 +0600

    add booster-buddy pixel code

--- resources/views/revamp/fuel-meter.blade.php
+++ resources/views/revamp/fuel-meter.blade.php
@@ -52,7 +52,7 @@
   <!-- Template Main CSS File -->
   <link href="{{ asset('landing2/assets/css/style.css', true) }}" rel="stylesheet">
 
-  <!-- Facebook Pixel Code -->
+  {{-- <!-- Facebook Pixel Code -->
   <script>
     !function(f,b,e,v,n,t,s)
   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -67,6 +67,26 @@
   </script>
   <noscript><img height="1" width="1" style="display:none"
       src="https://www.facebook.com/tr?id=265998565115584&ev=PageView&noscript=1" /></noscript>
+  <!-- End Facebook Pixel Code --> --}}
+
+  <!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window,document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+   fbq('init', '948521199337536'); 
+  fbq('track', 'PageView');
+  </script>
+  <noscript>
+   <img height="1" width="1" 
+  src="https://www.facebook.com/tr?id=948521199337536&ev=PageView
+  &noscript=1"/>
+  </noscript>
   <!-- End Facebook Pixel Code -->
 </head>
 
