c148ed84e5f4356a0429051ae636a6e786e7ea00
Shakil Ahmed <shakilbuet63@gmail.com>
2021-12-29 12:44:49 +0600

    add large_icon and banner_url in push send page

--- app/Http/Controllers/Test/PushNotificationController.php
+++ app/Http/Controllers/Test/PushNotificationController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Test;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Entities\Car;
@@ -9,90 +10,94 @@ use App\Service\Microservice\PushMicroservice;
 
 class PushNotificationController extends Controller
 {
-  public function test(Request $request)
-  {
-    $res = collect();
-    $reg_no = $request->reg_no;
-    if ($reg_no) {
-      $res->push('Starting diagnosis for: ' . $reg_no);
-      $car = Car::where('reg_no', $reg_no)->first();
-      
-      if (is_null($car)) {
-        $res->push('Car not found');
-      } else if (is_null($car->user)) {
-        $res->push('Car is not binded to any user');
-      } else {
-        $res->push('Car found in DB');
-        $res->push('Sending Push Notification to: ' . $car->user->name);
-        
-        $title = $request->get('title');
-        $body = $request->get('body');
-        $type = intval($request->get('type', '0'));
-        $data = [
-          'title' => $title,
-          'body' => $body,
-          'type' => $type,
-        ];
-        $service = new PushMicroservice();
-        $ret = $service->send($car->user_id, $data);
+    public function test(Request $request)
+    {
+        $res = collect();
+        $reg_no = $request->reg_no;
+        if ($reg_no) {
+            $res->push('Starting diagnosis for: ' . $reg_no);
+            $car = Car::where('reg_no', $reg_no)->first();
 
-        $socketRecipients = $ret['recipients']['socket'];
-        $oneSignalRecipients = $ret['recipients']['onesignal'];
+            if (is_null($car)) {
+                $res->push('Car not found');
+            } else if (is_null($car->user)) {
+                $res->push('Car is not binded to any user');
+            } else {
+                $res->push('Car found in DB');
+                $res->push('Sending Push Notification to: ' . $car->user->name);
 
-        $res->push('Notification Type: ' . $type);
-        $res->push('Notification sent to ' . ($socketRecipients + $oneSignalRecipients) . ' Mobile Devices');
-        $res->push('Via WebSocket = ' . $socketRecipients);
-        $res->push('Via OneSignal = ' . $oneSignalRecipients);
-      }
-      $res->push('Diagnosis finished');
-    }
-    
-    return view('test.push-noti')->with([
-      'messages' => $res,
-    ]);
-  }
+                $title = $request->get('title');
+                $body = $request->get('body');
+                $type = intval($request->get('type', '0'));
+                $data = [
+                    'title' => $title,
+                    'body' => $body,
+                    'type' => $type,
+                    'large_icon' => $request->get('large_icon', ''),
+                    'banner_url' => $request->get('banner_url', ''),
+                ];
 
-  private function trace($event, $listener) {
-    $arr = collect();
-    if (is_null($event->device->user)) {
-      $arr->push('Device is not connected with an user');
-      return $arr;
+                $service = new PushMicroservice();
+                $ret = $service->send($car->user_id, $data);
+
+                $socketRecipients = $ret['recipients']['socket'];
+                $oneSignalRecipients = $ret['recipients']['onesignal'];
+
+                $res->push('Notification Type: ' . $type);
+                $res->push('Notification sent to ' . ($socketRecipients + $oneSignalRecipients) . ' Mobile Devices');
+                $res->push('Via WebSocket = ' . $socketRecipients);
+                $res->push('Via OneSignal = ' . $oneSignalRecipients);
+            }
+            $res->push('Diagnosis finished');
+        }
+
+        return view('test.push-noti')->with([
+            'messages' => $res,
+        ]);
     }
 
-    $arr->push('Device is connected with user: ' . $event->device->user->name);
+    private function trace($event, $listener)
+    {
+        $arr = collect();
+        if (is_null($event->device->user)) {
+            $arr->push('Device is not connected with an user');
+            return $arr;
+        }
 
-    if (is_null($event->device->car)) {
-      $arr->push('Device is not connected with a car');
-      return $arr;
+        $arr->push('Device is connected with user: ' . $event->device->user->name);
+
+        if (is_null($event->device->car)) {
+            $arr->push('Device is not connected with a car');
+            return $arr;
+        }
+
+        $arr->push('Device is connected with car: ' . $event->device->car->reg_no);
+
+        $deliverable = $listener->deliverable($event->device->car, $event->limit, $event->flag);
+        if (!$deliverable) {
+            $arr->push('Notification is not deliverable');
+            return $arr;
+        }
+
+        $arr->push('Deliverable check is passed');
+
+        $data = $listener->payload($event);
+        $arr->push(json_encode($data));
+
+        $private = $event->device->user->customer_type == User::$CUSTOMER_PRIVATE;
+        $arr->push('Customer type is: ' . ($private ? 'private' : 'enterprise'));
+
+        if ($private && !($fl = $listener->privateValidate($event->device->user))) {
+            $arr->push('Private customer settings problem, speed notification = ' . $fl);
+            return $arr;
+        }
+
+        $arr->push('Private customer settings are ok');
+
+        $delivered = $listener->execute($event->device->user, $event->device, $data);
+
+        $arr->push('Notification delivery response: ' . $delivered);
+
+        return $arr;
     }
-
-    $arr->push('Device is connected with car: ' . $event->device->car->reg_no);
-
-    $deliverable = $listener->deliverable($event->device->car, $event->limit, $event->flag);
-    if (!$deliverable) {
-      $arr->push('Notification is not deliverable');
-      return $arr;
-    }
-
-    $arr->push('Deliverable check is passed');
-    
-    $data = $listener->payload($event);
-    $arr->push(json_encode($data));
-
-    $private = $event->device->user->customer_type == User::$CUSTOMER_PRIVATE;
-    $arr->push('Customer type is: ' . ($private ? 'private' : 'enterprise'));
-
-    if ($private && !($fl = $listener->privateValidate($event->device->user))) {
-      $arr->push('Private customer settings problem, speed notification = ' . $fl);
-      return $arr;
-    }
-
-    $arr->push('Private customer settings are ok');
-
-    $delivered = $listener->execute($event->device->user, $event->device, $data);
-
-    $arr->push('Notification delivery response: ' . $delivered);
-
-    return $arr;
-  }
 }
--- resources/views/test/push-noti.blade.php
+++ resources/views/test/push-noti.blade.php
@@ -2,44 +2,54 @@
 
 @section('content')
     <div class="row">
-      <div class="col-xs-12">
-        <h3>Test General Push Notification</h3>
-        <hr>
-        <form action="/test/push-noti" class="form col-xs-12 col-md-4" method="post">
-          {!! csrf_field() !!}
-          <div class="form-group">
-            <label for="reg">Reg. Number of a Car</label>
-            <input type="text" value="" name="reg_no" class="form-control" id="reg" placeholder="Ex: 99-1234" />
-          </div>
-          <div class="form-group">
-            <label for="noti-type">Notification Type</label>
-            <select name="type" value="0" class="form-control">
-              <option value="0">General</option>
-              <option value="20">Engine</option>
-              <option value="21">Speed</option>
-              <option value="22">Geo Fence</option>
-              <option value="23">Mileage</option>
-              <option value="24">Payment</option>
-              <option value="26">Password</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <label for="title">Notification Title</label>
-            <input type="text" value="" name="title" class="form-control" id="title" placeholder="A short title" />
-          </div>
-          <div class="form-group">
-            <label for="body">Notification Body</label>
-            <textarea type="text" value="" name="body" class="form-control" id="body" rows="6" placeholder="Write multiline text or Bangla text" ></textarea>
-          </div>
-          <button type="submit" class="btn btn-primary">Start Test</button>
-        </form>
-        <div class="col-xs-12 col-md-8">
-          <ol>
-            @foreach ($messages as $msg)
-                <li><strong>{{ $msg }}</strong></li>
-            @endforeach
-          </ol>
+        <div class="col-xs-12">
+            <h3>Test General Push Notification</h3>
+            <hr>
+            <form action="/test/push-noti" class="form col-xs-12 col-md-4" method="post">
+                {!! csrf_field() !!}
+                <div class="form-group">
+                    <label for="reg">Reg. Number of a Car</label>
+                    <input type="text" value="" name="reg_no" class="form-control" id="reg" placeholder="Ex: 99-1234" />
+                </div>
+                <div class="form-group">
+                    <label for="noti-type">Notification Type</label>
+                    <select name="type" value="0" class="form-control">
+                        <option value="0">General</option>
+                        <option value="20">Engine</option>
+                        <option value="21">Speed</option>
+                        <option value="22">Geo Fence</option>
+                        <option value="23">Mileage</option>
+                        <option value="24">Payment</option>
+                        <option value="26">Password</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="title">Notification Title</label>
+                    <input type="text" value="" name="title" class="form-control" id="title" placeholder="A short title" />
+                </div>
+                <div class="form-group">
+                    <label for="body">Notification Body</label>
+                    <textarea type="text" name="body" class="form-control" id="body" rows="6" placeholder="Write multiline text or Bangla text"></textarea>
+                </div>
+
+                <div class="form-group">
+                    <label for="large_icon">Large Icon</label>
+                    <input type="text" value="" name="large_icon" class="form-control" id="large_icon" placeholder="Large Icon Url" />
+                </div>
+                <div class="form-group">
+                    <label for="banner_url">Notification Title</label>
+                    <input type="text" value="" name="banner_url" class="form-control" id="banner_url" placeholder="Banner Url" />
+                </div>
+
+                <button type="submit" class="btn btn-primary">Start Test</button>
+            </form>
+            <div class="col-xs-12 col-md-8">
+                <ol>
+                    @foreach ($messages as $msg)
+                        <li><strong>{{ $msg }}</strong></li>
+                    @endforeach
+                </ol>
+            </div>
         </div>
-      </div>
     </div>
 @endsection
