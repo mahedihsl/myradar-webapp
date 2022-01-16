0378ae53fe0a91f1d9454689b3d548deff327ae7
Shakil Ahmed <shakilbuet63@gmail.com>
2022-01-16 17:26:46 +0600

    reformat

--- config/microservice.php
+++ config/microservice.php
@@ -1,127 +1,128 @@
 <?php
 
 return [
-  'geofence' => [
-    'url' => 'http://myradar-geofence',
-    'port' => 6000,
-  ],
-  'push' => [
-      'url' => 'http://myradar-push',
-      'port' => 6001,
-  ],
-  'sms' => [
-      'url' => 'http://myradar-sms',
-      'port' => 6002,
-  ],
-  'user' => [
-      'url' => 'http://myradar-user',
-      'port' => 6003,
-  ],
-  'device' => [
-    'url' => 'http://myradar-device',
-    'port' => 6004,
-  ],
-  'health' => [
-    'url' => 'http://myradar-health',
-    'port' => 6005,
-  ],
-  'socket' => [
-    'url' => 'http://myradar-socket',
-    'port' => 6007,
-  ],
-  'et200' => [
-    'url' => 'http://concox-et200',
-    'port' => 5051,
-  ],
-  'jt808' => [
-    'url' => 'http://jt808',
-    'port' => 5053,
-  ],
-  'speed' => [
-    'url' => 'http://myradar-speed',
-    'port' => 6010,
-  ],
-  'mileage' => [
-    'url' => 'http://myradar-mileage',
-    'port' => 6011,
-  ],
-  'car' => [
-    'url' => 'http://myradar-car',
-    'port' => 6012,
-  ],
-  'fuel' => [
-    'url' => 'http://myradar-fuel',
-    'port' => 6013,
-  ],
-  'supervisor' => [
-    'url' => 'http://myradar-supervisor',
-    'port' => 6014,
-  ],
-  'supervisor-aws' => [
-    'url' => 'http://54.255.200.156',
-    'port' => 6014,
-  ],
-  'poi' => [
-    'url' => 'http://myradar-poi',
-    'port' => 6015,
-  ],
-  'location' => [
-    'url' => 'http://myradar-location',
-    'port' => 6016,
-  ],
-  'trip' => [
-    'url' => 'http://myradar-trip',
-    'port' => 6017,
-  ],
-  'demo' => [
-    'url' => 'http://myradar-demo',
-    'port' => 6018,
-  ],
-  'promotion' => [
-    'url' => 'http://myradar-promotion',
-    'port' => 6019,
-  ],
-  // 6020 is for the WordPress website
-  'payment' => [
-    'url' => 'http://myradar-payment',
-    'port' => 6021,
-  ],
-  
-  'radar-recharge' => [
-    'url' => 'http://radar-api',
-    'port' => 7000,
-  ],
-  'msgid' => [
-    'url' => 'http://myradar-msgid',
-    'port' => 7001,
-  ],
-  
-  'rms-receiver' => [
-    'url' => 'http://rms-receiver',
-    'port' => 5000,
-  ],
-  'rms-mains' => [
-    'url' => 'http://rms-mains',
-    'port' => 5001,
-  ],
-  'rms-dg' => [
-    'url' => 'http://rms-dg',
-    'port' => 5002,
-  ],
-  'rms-battery' => [
-    'url' => 'http://rms-battery',
-    'port' => 5003,
-  ],
-  'rms-user' => [
-    'url' => 'http://rms-user',
-    'port' => 5004,
-  ],
-  'rms-temperature' => [
-    'url' => 'http://rms-temperature',
-    'port' => 5005,
-  ],
-  'rms-door' => [
-    'url' => 'http://rms-door',
-    'port' => 5006,
-  ],
+    'geofence' => [
+        'url' => 'http://myradar-geofence',
+        'port' => 6000,
+    ],
+    'push' => [
+        'url' => 'http://myradar-push',
+        'port' => 6001,
+    ],
+    'sms' => [
+        'url' => 'http://myradar-sms',
+        'port' => 6002,
+    ],
+    'user' => [
+        'url' => 'http://myradar-user',
+        'port' => 6003,
+    ],
+    'device' => [
+        'url' => 'http://myradar-device',
+        'port' => 6004,
+    ],
+    'health' => [
+        'url' => 'http://myradar-health',
+        'port' => 6005,
+    ],
+    'socket' => [
+        'url' => 'http://myradar-socket',
+        'port' => 6007,
+    ],
+    'et200' => [
+        'url' => 'http://concox-et200',
+        'port' => 5051,
+    ],
+    'jt808' => [
+        'url' => 'http://jt808',
+        'port' => 5053,
+    ],
+    'speed' => [
+        'url' => 'http://myradar-speed',
+        'port' => 6010,
+    ],
+    'mileage' => [
+        'url' => 'http://myradar-mileage',
+        'port' => 6011,
+    ],
+    'car' => [
+        'url' => 'http://myradar-car',
+        'port' => 6012,
+    ],
+    'fuel' => [
+        'url' => 'http://myradar-fuel',
+        'port' => 6013,
+    ],
+    'supervisor' => [
+        'url' => 'http://myradar-supervisor',
+        'port' => 6014,
+    ],
+    'supervisor-aws' => [
+        'url' => 'http://54.255.200.156',
+        'port' => 6014,
+    ],
+    'poi' => [
+        'url' => 'http://myradar-poi',
+        'port' => 6015,
+    ],
+    'location' => [
+        'url' => 'http://myradar-location',
+        'port' => 6016,
+    ],
+    'trip' => [
+        'url' => 'http://myradar-trip',
+        'port' => 6017,
+    ],
+    'demo' => [
+        'url' => 'http://myradar-demo',
+        'port' => 6018,
+    ],
+    'promotion' => [
+        'url' => 'http://myradar-promotion',
+        'port' => 6019,
+    ],
+    // 6020 is for the WordPress website
+    'payment' => [
+        'url' => 'http://myradar-payment',
+        'port' => 6021,
+    ],
+    // 6022 is for the myradar-demo service
+
+    'radar-recharge' => [
+        'url' => 'http://radar-api',
+        'port' => 7000,
+    ],
+    'msgid' => [
+        'url' => 'http://myradar-msgid',
+        'port' => 7001,
+    ],
+
+    'rms-receiver' => [
+        'url' => 'http://rms-receiver',
+        'port' => 5000,
+    ],
+    'rms-mains' => [
+        'url' => 'http://rms-mains',
+        'port' => 5001,
+    ],
+    'rms-dg' => [
+        'url' => 'http://rms-dg',
+        'port' => 5002,
+    ],
+    'rms-battery' => [
+        'url' => 'http://rms-battery',
+        'port' => 5003,
+    ],
+    'rms-user' => [
+        'url' => 'http://rms-user',
+        'port' => 5004,
+    ],
+    'rms-temperature' => [
+        'url' => 'http://rms-temperature',
+        'port' => 5005,
+    ],
+    'rms-door' => [
+        'url' => 'http://rms-door',
+        'port' => 5006,
+    ],
 ];
