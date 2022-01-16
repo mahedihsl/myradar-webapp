6b075034402578e71f12dc3331ef2079d3c48855
Shakil Ahmed <shakilbuet63@gmail.com>
2022-01-16 17:28:59 +0600

    find user by static token from user service

--- app/Service/Microservice/UserMicroservice.php
+++ app/Service/Microservice/UserMicroservice.php
@@ -4,83 +4,96 @@ namespace App\Service\Microservice;
 
 class UserMicroservice extends BaseService
 {
-  const SERVICE_NAME = 'user';
+    const SERVICE_NAME = 'user';
 
-  public function __construct() {
-    parent::__construct(self::SERVICE_NAME);
-  }
+    public function __construct()
+    {
+        parent::__construct(self::SERVICE_NAME);
+    }
 
-  public function disableByCar($reg_no)
-  {
-    return $this->post('/account/disable/by-car', compact('reg_no'));
-  }
+    public function getAuthUser($api_token)
+    {
+        return $this->get('/auth/api-user-v1', compact('api_token'));
+    }
 
-  public function toggleStatus($user_id, $actor_id) {
-    return $this->post('/account/toggle/status', compact('user_id', 'actor_id'));
-  }
+    public function disableByCar($reg_no)
+    {
+        return $this->post('/account/disable/by-car', compact('reg_no'));
+    }
 
-  public function statusHistory($user_id) {
-    return $this->get('/account/status/history', compact('user_id'));
-  }
+    public function toggleStatus($user_id, $actor_id)
+    {
+        return $this->post('/account/toggle/status', compact('user_id', 'actor_id'));
+    }
 
-  public function registerSession($data) {
-    return $this->post('/session/save', $data);
-  }
+    public function statusHistory($user_id)
+    {
+        return $this->get('/account/status/history', compact('user_id'));
+    }
 
-  public function removeSessionsOfUser($data) {
-    return $this->post('/session/remove', $data);
-  }
+    public function registerSession($data)
+    {
+        return $this->post('/session/save', $data);
+    }
 
-  public function removeSessionById($session_id) {
-    return $this->post('/session/remove-by-id', compact('session_id'));
-  }
+    public function removeSessionsOfUser($data)
+    {
+        return $this->post('/session/remove', $data);
+    }
 
-  public function login($data)
-  {
-    return $this->post('/auth/login', $data);
-  }
-  
-  public function token($user_id)
-  {
-    return $this->post('/auth/token', compact('user_id'));
-  }
-  
-  public function refresh($refresh_token)
-  {
-    return $this->post('/auth/refresh', compact('refresh_token'));
-  }
+    public function removeSessionById($session_id)
+    {
+        return $this->post('/session/remove-by-id', compact('session_id'));
+    }
 
-  public function profile($token)
-  {
-    $headers = ['authorization' => $token];
-    return $this->post('/auth/profile', [], $headers);
-  }
+    public function login($data)
+    {
+        return $this->post('/auth/login', $data);
+    }
 
-  public function forceLogout($session_id) {
-    return $this->post('/session/force-logout', compact('session_id'));
-  }
+    public function token($user_id)
+    {
+        return $this->post('/auth/token', compact('user_id'));
+    }
 
-  public function getSessionList($user_id)
-  {
-    return $this->get('/session/list', compact('user_id'));
-  }
+    public function refresh($refresh_token)
+    {
+        return $this->post('/auth/refresh', compact('refresh_token'));
+    }
 
-  public function logPasswordChange($user_id) {
-    return $this->post('/session/password-changed', compact('user_id'));
-  }
+    public function profile($token)
+    {
+        $headers = ['authorization' => $token];
+        return $this->post('/auth/profile', [], $headers);
+    }
 
-  public function scanUnderengagedUsers()
-  {
-    return $this->post('/engage/sms-pack1-enable');
-  }
+    public function forceLogout($session_id)
+    {
+        return $this->post('/session/force-logout', compact('session_id'));
+    }
 
-  public function testMileagePush($user_id)
-  {
-    return $this->get('/test/mileage-push', compact('user_id'));
-  }
+    public function getSessionList($user_id)
+    {
+        return $this->get('/session/list', compact('user_id'));
+    }
 
-  public function test()
-  {
-    return $this->get('/test', [ 'user' => 'palatok' ]);
-  }
+    public function logPasswordChange($user_id)
+    {
+        return $this->post('/session/password-changed', compact('user_id'));
+    }
+
+    public function scanUnderengagedUsers()
+    {
+        return $this->post('/engage/sms-pack1-enable');
+    }
+
+    public function testMileagePush($user_id)
+    {
+        return $this->get('/test/mileage-push', compact('user_id'));
+    }
+
+    public function test()
+    {
+        return $this->get('/test', ['user' => 'palatok']);
+    }
 }
