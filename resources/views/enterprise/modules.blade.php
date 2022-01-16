6a7e36300d9ed77f61110f680841705ae8672aaf
Shakil Ahmed <shakilbuet63@gmail.com>
2022-01-16 17:30:41 +0600

    put HSL name to demo enterprise module

--- resources/views/enterprise/modules.blade.php
+++ resources/views/enterprise/modules.blade.php
@@ -14,15 +14,23 @@
   <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
-  <div class="w-full h-screen bg-blue-600 flex flex-row justify-center items-center space-x-8">
-    <a href="/car/tracking" class="w-1/3 flex flex-col items-center justify-center space-y-6 py-6 bg-white rounded-xl shadow-xl hover:bg-gray-100 duration-300">
-      <span class="text-gray-600 font-bold text-xl">Fleet Management</span>
-      <img src="/images/redcar.svg" class="w-1/2 h-auto" alt="">
-    </a>
-    <a href="http://182.160.99.137:6022/vessel/tracking" target="_blank" class="w-1/3 flex flex-col items-center justify-center space-y-6 py-6 bg-white rounded-xl shadow-xl hover:bg-gray-100 duration-300">
-      <span class="text-gray-600 font-bold text-xl">Vessel Management</span>
-      <img src="/images/launch.svg" class="w-1/2 h-auto" alt="">
-    </a>
+  <div class="w-full h-screen bg-blue-600 flex flex-col justify-center items-center space-y-12">
+      <div class="w-full flex flex-row justify-center items-center space-x-8">
+          <a href="/car/tracking" class="w-1/3 flex flex-col items-center justify-center space-y-6 py-6 bg-white rounded-xl shadow-xl hover:bg-gray-100 duration-300">
+              <span class="text-gray-600 font-bold text-xl">Fleet Management</span>
+              <img src="/images/redcar.svg" class="w-1/2 h-auto" alt="">
+          </a>
+          <a href="http://biwta.myradar.com.bd/login?token=7F5C119F2812E4D795E95E2649BD2" target="_blank" class="w-1/3 flex flex-col items-center justify-center space-y-6 py-6 bg-white rounded-xl shadow-xl hover:bg-gray-100 duration-300">
+              <span class="text-gray-600 font-bold text-xl">Vessel Management</span>
+              <img src="/images/launch.svg" class="w-1/2 h-auto" alt="">
+          </a>
+      </div>
+      <div class="w-full flex flex-col items-center">
+          <p class="text-base text-white font-light">Developed By</p>
+          <p class="text-3xl text-white hover:text-gray-800 font-semibold">
+              <a href="http://hypersystems.com.bd." target="_blank">Hyper Systems Ltd.</a>
+          </p>
+      </div>
   </div>
 </body>
 </html>
