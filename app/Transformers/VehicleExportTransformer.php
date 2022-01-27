71054ad7d3d9e89d9d847a3da20c2bd3b7a0ddbb
Shakil Ahmed <shakilbuet63@gmail.com>
2022-01-27 16:11:31 +0600

    handle last_pulse non existence

--- app/Transformers/VehicleExportTransformer.php
+++ app/Transformers/VehicleExportTransformer.php
@@ -29,7 +29,11 @@ class VehicleExportTransformer extends TransformerAbstract
         if ( ! is_null($device)) {
             $phone = $device->phone;
             $com_id = $device->com_id;
-            $last_pulse = $device->last_pulse_label;
+            try {
+                $last_pulse = $device->last_pulse_label;
+            } catch (\Exception $e) {
+                //throw $th;
+            }
         }
 
         return [
